f41a9d87e145459155242915f06ab6fb6d93d357
Tong Shen <tshen70@gatech.edu>
2021-10-22 23:25:41 -0700

    image upload and store implemented

--- Server/client_upload.py
+++ Server/client_upload.py
@@ -1,8 +1,8 @@
 import requests
-
+import json
+import base64
 
 url = 'http://127.0.0.1:5000/'
-my_img = {'image': open('1.jpg', 'rb')}
-r = requests.post(url, files=my_img)
-
+files = {'image': open('1.jpg', 'rb')}
+r = requests.post(url, files=files)
 print(r.json())
