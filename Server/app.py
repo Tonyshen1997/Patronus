dbd775b1803dd922be55247f1d5b39cbd73ffad0
Tong Shen <tshen70@gatech.edu>
2021-10-23 14:37:30 -0700

    Server and client socket implemented

--- Server/app.py
+++ Server/app.py
@@ -2,6 +2,12 @@
 import os
 from flask import Flask, request, jsonify
 
+
+# The `project` parameter is optional and represents which project the client
+# will act on behalf of. If not supplied, the client falls back to the default
+# project inferred from the environment.
+
+
 UPLOAD_FOLDER = '/Users/TonyS/Project/Patronus/Patronus/Server/Upload'
 ALLOWED_EXTENSIONS = set('jpg')
 
@@ -12,6 +18,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def upload_image():
     if request.method == 'POST':
         image = request.files['image']
+
         image.save(os.path.join(app.config['UPLOAD_FOLDER'], '2.jpg'))
 
     return jsonify({'msg': 'success'})
--- Server/client_upload.py
+++ Server/client_upload.py
@@ -2,7 +2,7 @@ import requests
 import json
 import base64
 
-url = 'http://127.0.0.1:5000/'
+url = 'http://34.145.220.144'
 files = {'image': open('1.jpg', 'rb')}
 r = requests.post(url, files=files)
 print(r.json())
